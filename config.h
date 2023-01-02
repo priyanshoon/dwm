ea25a9cacb0fbb0f8f231655078a1505659c099c
Priyanshu Gupta <guptapriyanshu973@gmail.com>
2023-01-02 20:18:21 +0530

    yoyooyoy

--- config.h
+++ config.h
@@ -1,22 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11.5" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11.5";
+static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#eeeeee";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_darkpink[]    = "#5d003a";
 static const char col_darkgray[]    = "#43464b";
 static const char col_darkpurple[]  = "#230041";
 static const char col_darkgreen[]   = "#076f3b";
+static const char col_brown[]       = "#bdae93";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -24,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { ">_", "www", "^_^", "{/}", "@@", "##" };
+static const char *tags[] = { ">_", "www", "^_^", "{/}", "@@", "##", "$$" };
 
 static const Rule rules[] = {
 	/* xprop(1):
